fe7ba290214028ed67d3810356bfb1067a93b887
stempz101 <kms111001@gmail.com>
2023-11-12 15:03:36 +0200

    changed group id for anime-ms

--- anime-ms/src/main/java/org/stempz/fanime/animems/AnimeMSApplication.java
+++ anime-ms/src/main/java/org/stempz/fanime/animems/AnimeMSApplication.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.stempz.fanime.animems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ anime-ms/src/test/java/org/stempz/fanime/animems/AnimeMSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stempz.fanime.animems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AnimeMSApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
