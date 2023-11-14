d9ba2934622da59e15b7285b8c0faaf83edb6b23
stempz101 <kms111001@gmail.com>
2023-11-14 19:51:59 +0200

    init tests in discovery-ms

---
+++ discovery-ms/src/test/java/org/stempz/fanime/discoveryms/DiscoveryMSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stempz.fanime.discoveryms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DiscoveryMSApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
