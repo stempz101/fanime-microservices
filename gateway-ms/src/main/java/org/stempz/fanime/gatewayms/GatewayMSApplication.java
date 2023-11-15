e05dab9ae7849f162add1c127a9f953bcd234349
stempz101 <kms111001@gmail.com>
2023-11-15 15:06:59 +0200

    configured spring cloud gateway

---
+++ gateway-ms/src/main/java/org/stempz/fanime/gatewayms/GatewayMSApplication.java
@@ -0,0 +1,12 @@
+package org.stempz.fanime.gatewayms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMSApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayMSApplication.class, args);
+  }
+}
---
+++ gateway-ms/src/test/java/org/stempz/fanime/gatewayms/GatewayMSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stempz.fanime.gatewayms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GatewayMSApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
