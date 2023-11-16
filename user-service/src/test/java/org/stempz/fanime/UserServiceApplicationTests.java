8fa86f2a6d510d53e158156d2125497f63f3adf0
stempz101 <kms111001@gmail.com>
2023-11-16 12:37:19 +0200

    created user-service

---
+++ user-service/src/test/java/org/stempz/fanime/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stempz.fanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
