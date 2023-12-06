4ee9b97d754bdc8b0d9c8b39e3547e874649e270
stempz101 <kms111001@gmail.com>
2023-12-06 21:13:14 +0200

    anime: changed 'animes' to 'anime' in @RequestMapping

--- anime-service/src/main/java/org/stempz/fanime/animeservice/controller/AnimeController.java
+++ anime-service/src/main/java/org/stempz/fanime/animeservice/controller/AnimeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.stempz.fanime.animeservice.dto.AnimeBrowseDto;
 
 @RestController
-@RequestMapping("/api/v1/animes")
+@RequestMapping("/api/v1/anime")
 public class AnimeController {
 
   @GetMapping
